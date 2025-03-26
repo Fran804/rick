a7e8a239c530688d72e388fbc7e129fb126075e3
Fran804 <franciscovillalobos804@gmail.com>
2025-03-26 01:05:21 -0600

    Termine opiniones y avance formulario unete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './pages/hero/hero.component';
 import { AboutComponent } from './pages/about/about.component';
 import { CharactersComponent } from './pages/characters/characters.component';
+import { OpinionsComponent } from './pages/opinions/opinions.component';
+import { JoinClubComponent } from './pages/join-club/join-club.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HeroComponent, AboutComponent, CharactersComponent],
+  imports: [NavbarComponent, HeroComponent, AboutComponent, CharactersComponent, OpinionsComponent, JoinClubComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
